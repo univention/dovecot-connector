4d5c48b5c9bc8161300b17a3de4d56f7599a6698
Per Macha <macha@univention.de>
2022-01-17 00:00:00 +0100

    feat(get-artifact): check if job has artifacts [iam-issues#183]

--- get-artifact.py
+++ get-artifact.py
@@ -6,6 +6,7 @@ Download an artifact from a different Gitlab project
 """
 
 # included batteries
+from datetime import datetime
 import os
 import sys
 
@@ -34,15 +35,18 @@ class GitlabApi:
         """Get the latest pipeline-id of a given branch"""
         pipelines_url = f'{self.base_url}/projects/{self.project_id}/pipelines'
         params = {'ref': branch, 'per_page': 1}
-        response1 = self.session.get(pipelines_url, params=params)
-        content = response1.json()
-        if response1.status_code == 401:
+        response = self.session.get(pipelines_url, params=params)
+        content = response.json()
+        if response.status_code == 401:
             print(f"Message: {content['message']}")
             raise GitlabError('Bad authorization. Check token!')
-        if response1.status_code == 404:
+        if response.status_code == 404:
             print(f"Message: {content['message']}")
             raise GitlabError('Project not found')
-        pipeline_id = response1.json()[0]['id']
+        if response.status_code != 200:
+            print(f"Message: {content['message']}")
+            raise GitlabError(f'Request failed with {response.status_code}')
+        pipeline_id = response.json()[0]['id']
         return pipeline_id
 
     def get_job_id(self, pipeline_id, job_name='build-job'):
@@ -51,12 +55,33 @@ class GitlabApi:
             f'{self.base_url}/projects/{self.project_id}'
             f'/pipelines/{pipeline_id}/jobs'
         )
-        response2 = self.session.get(jobs_url)
-        jobs = response2.json()
+        response = self.session.get(jobs_url)
+        if response.status_code != 200:
+            raise GitlabError(f'Request failed with {response.status_code}')
+        jobs = response.json()
         for job in jobs:
             if job['name'] == job_name:
                 job_id = job['id']
         return job_id
+
+    def ensure_archive_availability(self, job_id):
+        """Test if the current job has a downloadable archive"""
+        job_url = f'{self.base_url}/projects/{self.project_id}/jobs/{job_id}'
+        response = self.session.get(job_url)
+        if response.status_code != 200:
+            raise GitlabError(f'Request failed with {response.status_code}')
+        job = response.json()
+        if 'artifacts_file' not in job or 'artifacts_expire_at' not in job:
+            raise GitlabError('archive not found')
+        artifacts_expire_at = datetime.strptime(
+            job["artifacts_expire_at"],
+            '%Y-%m-%dT%H:%M:%S.%fZ',
+        )
+        time_left = artifacts_expire_at - datetime.utcnow()
+        seconds_left = time_left.total_seconds()
+        if seconds_left < 0:
+            raise GitlabError('artifacts expired')
+        return
 
     def download_artifact(self, job_id, download_path=DEFAULT_DL_PATH):
         """Download the artifact zip-file of a given job"""
@@ -64,10 +89,13 @@ class GitlabApi:
             f'{self.base_url}/projects/{self.project_id}'
             f'/jobs/{job_id}/artifacts'
         )
-        with self.session.get(artifacts_url, stream=True) as response3:
-            response3.raise_for_status()
+        with self.session.get(artifacts_url, stream=True) as response:
+            if response.status_code != 200:
+                raise GitlabError(
+                    f'Request failed with {response.status_code}'
+                )
             with open(download_path, 'wb') as buffered_writer:
-                for chunk in response3.iter_content(chunk_size=8192):
+                for chunk in response.iter_content(chunk_size=8192):
                     buffered_writer.write(chunk)
         return
 
@@ -81,18 +109,43 @@ def main(
 ):
     """Get latest pipeline-id, builders-job-id and download the artifact"""
     if gitlab_token is None or not gitlab_token:
-        print('Token is missing')
+        print('Token is missing. Please set GLDL_TOKEN as env-var!')
         return 1
+
     api = GitlabApi(
         ca_path, gitlab_api_base_url, gitlab_project_id, gitlab_token
     )
+
     try:
         pipeline_id = api.get_pipeline_id()
     except GitlabError as err:
-        print(f'Failed: {err}')
+        print(f'Failed to retrieve pipeline-id: {err}')
         return 2
-    job_id = api.get_job_id(pipeline_id)
-    api.download_artifact(job_id, download_path)
+
+    try:
+        job_id = api.get_job_id(pipeline_id)
+    except GitlabError as err:
+        print(f'Failed to retrieve job-id: {err}')
+        return 3
+
+    try:
+        api.ensure_archive_availability(job_id)
+    except GitlabError as err:
+        print(
+            f'Failed to retrieve archive for pipeline {pipeline_id} '
+            f'/ job {job_id}: {err}'
+        )
+        print(
+            'Please check if the pipeline of the main branch succeeded lately!'
+        )
+        return 4
+
+    try:
+        api.download_artifact(job_id, download_path)
+    except GitlabError as err:
+        print(f'Failed to download artifact: {err}')
+        return 5
+
     return 0
 
 
